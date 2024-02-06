d147e0cf260f9554870049b4163d20b85d756f11
luqisha <ashiqul.islam12@gmail.com>
2024-02-06 21:28:06 +0600

    Error Fixed, Removed CEG & FOX ticker.

--- modules/strategy.py
+++ modules/strategy.py
@@ -7,11 +7,11 @@ def price_behaviour(data):
 
 def group4_ensemble_model_signals(df):
 
-    behaviour = price_behaviour(df.close)
+    hurst_exp = price_behaviour(df.close)
 
-    if behaviour > 0.5:
+    if hurst_exp > 0.5:
         print(f"{df.index[0]} -- {df.index[-1]} -- Price behaviour is Trend Following")
-    elif behaviour < 0.5:
+    elif hurst_exp < 0.5:
         print(f"{df.index[0]} -- {df.index[-1]} -- Price behaviour is Mean Reverting")
     else:
         print(f"{df.index[0]} -- {df.index[-1]} -- Price behaviour is Random")
@@ -19,9 +19,9 @@ def group4_ensemble_model_signals(df):
     macd_sigs = macd_signals(df, 12, 26, 9)
     bb_sigs = bollinger_bands(df, 20, 2, sigs=True)
 
-    if behaviour > 0.5:
+    if hurst_exp > 0.5:
         signal = macd_sigs[-1]
-    elif behaviour < 0.5:
+    elif hurst_exp < 0.5:
         signal = bb_sigs[-1]
     else:
         signal = 0
